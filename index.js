91a900773f291a1678ba92404da6dfda37eafe6d
w7br <agenciavks@gmail.com>
2024-04-16 11:09:32 -0300

    vamos que vamos haha

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("jornada ocean abril");
+const express = require('express')
+
+const app = express()
+
+ 
+
+app.get('/', function (req, res) {
+
+  res.send('Hello World')
+
+})
+
+ 
+
+app.listen(3000)
