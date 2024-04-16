5319eb88a5f70faba8ff709c47b2ac92cc73cd6a
w7br <agenciavks@gmail.com>
2024-04-16 11:13:54 -0300

    alterando a porta para não conflitar com a aplicação do nextjs que está em desenvolvimento ...

--- index.js
+++ index.js
@@ -1,15 +1,8 @@
-const express = require('express')
-
-const app = express()
-
- 
+const express = require('express');
+const app = express(); 
 
 app.get('/', function (req, res) {
+  res.send('Hello World');
+});
 
-  res.send('Hello World')
-
-})
-
- 
-
-app.listen(3000)
+app.listen(8000);
