752f65377743b2c45a57d1921a47d709bad0bbbc
w7br <agenciavks@gmail.com>
2024-04-17 10:27:56 -0300

    add nodemon x1

--- index.js
+++ index.js
@@ -6,7 +6,16 @@ app.get('/', function (req, res) {
 });
 
 app.get('/oi', function (req, res) {
-    res.send('Olá Mundo');
-  });
+   res.send('Olá Mundo');
+});
+
+// lista de itens
+const lista = ['NextJS', 'NodeJS', 'Firebase'];
+
+//endpoit Read All => GET /item
+app.get('/item', function(req, res){
+  res.send(lista)
+})
+
 
 app.listen(8000);
